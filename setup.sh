51c90a3087245047accd22770a2204977e1a44fc
Carlos Cerqueira <ce.cesc01@gmail.com>
2024-06-23 22:12:55 -0300

    feat: add comms interfaces

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@ set -e
 
 git submodule update --init --recursive
 sudo apt-get update
+sudo apt install python3-pip
+pip3 install numpy catkin_pkg empy==3.3.4
+pip3 install lark --prefer-binary
 rosdep update --rosdistro=$ROS_DISTRO
 rosdep install --from-paths src --ignore-src -r -i -y --rosdistro=$ROS_DISTRO
