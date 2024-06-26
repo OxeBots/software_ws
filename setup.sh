32bf5b4000a4ce696b8d48cbbe9a9d22688004e3
Carlos Cerqueira <ce.cesc01@gmail.com>
2024-06-25 21:54:53 -0300

    feat: add comms pkg

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 git submodule update --init --recursive
 sudo apt-get update
-sudo apt install python3-pip
+sudo apt install python3-pip protobuf-compiler libprotobuf-dev
 pip3 install numpy catkin_pkg empy==3.3.4
 pip3 install lark --prefer-binary
 rosdep update --rosdistro=$ROS_DISTRO
