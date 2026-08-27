66cae6979a36fc7766538537ffb79a7b3c8e1513
Paulo Azul <paulotelesserraazul@gmail.com>
2026-08-27 12:13:37 +0000

    atualização dos ponteiros dos submodulos e configuracoes root

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ git submodule update --init --recursive
 sudo apt-get update
 rosdep update --rosdistro=$ROS_DISTRO
 rosdep install --from-paths src --ignore-src -r -i -y --rosdistro=$ROS_DISTRO
+sudo chmod 666 /dev/ttyUSB*
+sudo usermod -aG dialout $USER
