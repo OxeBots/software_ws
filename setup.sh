78f879bc7fc21e9db9a692af859be9ab502db9d4
Paulo Azul <paulotelesserraazul@gmail.com>
2026-09-09 19:37:12 +0000

    add suporte CLION e correção setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,12 @@ rosdep update --rosdistro=$ROS_DISTRO
 rosdep install --from-paths src --ignore-src -r -i -y --rosdistro=$ROS_DISTRO
 sudo chmod 666 /dev/ttyUSB*
 sudo usermod -aG dialout $USER
+
+sudo apt-get update && sudo apt-get install -y wget && \
+cd /tmp && \
+wget https://github.com/microsoft/onnxruntime/releases/download/v1.19.2/onnxruntime-linux-x64-1.19.2.tgz && \
+sudo tar -xzf onnxruntime-linux-x64-1.19.2.tgz -C /usr/local --strip-components=1 && \
+rm onnxruntime-linux-x64-1.19.2.tgz && \
+sudo ldconfig
+
+cd ../software_ws/
